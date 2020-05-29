8ef37745c175c2778d21ffc479b22f798cb11f88
SwagMint <23306536+PaleMintBlue@users.noreply.github.com>
2020-05-28 21:45:23 -0400

    Update 144. Binary Tree Preorder Traversal.java

--- Tree/144. Binary Tree Preorder Traversal.java
+++ Tree/144. Binary Tree Preorder Traversal.java
@@ -14,6 +14,18 @@
  * }
  */
 class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        recursive(root, res);
+        return res;
+    }
+    public void recursive(TreeNode node, List<Integer> res) {
+        if(node == null) return;
+        res.add(node.val);
+        recursive(node.left, res);
+        recursive(node.right, res);
+    }
+    
     public List<Integer> iterative(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if(root == null) return res;
