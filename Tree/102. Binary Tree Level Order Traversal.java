d4c1a38d50402589495663a9925d76edb07f7531
SwagMint <23306536+PaleMintBlue@users.noreply.github.com>
2020-05-29 23:21:52 -0400

    Update 102. Binary Tree Level Order Traversal.java

--- Tree/102. Binary Tree Level Order Traversal.java
+++ Tree/102. Binary Tree Level Order Traversal.java
@@ -14,9 +14,11 @@
  * }
  */
 class Solution {
-    List<List<Integer>> levels = new ArrayList<List<Integer>>();
     
-    public void helper(TreeNode root) {
+    /** Recursive
+    */
+    List<List<Integer>> levels = new ArrayList<List<Integer>>();
+    private void helper(TreeNode root) {
         if (levels.size() == level) levels.add(new ArrayList<Integer>());
 
         levels.get(level).add(node.val);
@@ -28,6 +30,29 @@ class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         if (root == null) return levels;
         helper(root, 0);
+        return levels;
+    }
+    
+    /** Iterative
+    */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<List<Integer>>();
+        if (root == null) return levels;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+        while ( !queue.isEmpty() ) {
+          levels.add(new ArrayList<Integer>());
+          int level_length = queue.size();
+          for(int i = 0; i < level_length; i++) {
+            TreeNode node = queue.remove();
+            levels.get(level).add(node.val);
+            if (node.left != null) queue.add(node.left);
+            if (node.right != null) queue.add(node.right);
+          }
+          level++;
+        }
         return levels;
     }
 }
