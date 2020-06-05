4c82d974dd8b47fd0e27ace9f7c69bae16407cd5
SwagMint <23306536+PaleMintBlue@users.noreply.github.com>
2020-06-05 02:23:02 -0400

    Create 280. Wiggle Sort.java

---
+++ Search & Sort/280. Wiggle Sort.java
@@ -0,0 +1,16 @@
+
+// class Solution {
+//     public void wiggleSort(int[] nums) {
+//         for (int i = 0; i < nums.length - 1; i++) {
+//             if ((i % 2 == 0) == (nums[i] > nums[i + 1])) {
+//                 swap(nums, i, i + 1);
+//             }
+//         }
+//     }
+    
+//     private void swap(int[] arr, int p1, int p2) {
+//         int tmp = arr[p1];
+//         arr[p1] = arr[p2];
+//         arr[p2] = tmp;
+//     }
+// }
