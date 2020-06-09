f52d0068da1875b7d40bb987137b3a6c74563d10
SwagMint <23306536+PaleMintBlue@users.noreply.github.com>
2020-06-08 23:24:49 -0400

    Create 300. Longest Increasing Subsequence.java

---
+++ Search & Sort/300. Longest Increasing Subsequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums) {
+            int i = biSearch(dp, 0, len, num);
+            if (i < 0) i = -(i + 1);
+            dp[i] = num;
+            if (i == len) len++;
+        }
+        return len;
+    }
+    
+    private int biSearch(int[] arr, int lo, int hi, int k) {
+        if (arr.length == 0) return -1;
+        hi = hi - 1;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) >>> 1; 
+            if (arr[mid] < k) lo = mid + 1;
+            else if (arr[mid] > k) hi = mid - 1;
+            else return mid;
+        }
+        return ~lo;
+    }
+}
