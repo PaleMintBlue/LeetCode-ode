a97624cab47a37e7a78fbb327afc528ad7ee9ec6
SwagMint <23306536+PaleMintBlue@users.noreply.github.com>
2020-06-10 17:32:03 -0400

    Update 547. Friend Circles.java

--- Graph/547. Friend Circles.java
+++ Graph/547. Friend Circles.java
@@ -1,7 +1,7 @@
 class Solution {
      /** DFS
-     * T: log(n^2)
-     * S: log(n)
+     * T: log(n^2), S: log(n)
+     */
      public int findCircleNum(int[][] M) {
         int[] visited = new int[M.length];
         int count = 0;
@@ -22,5 +22,31 @@ class Solution {
             }
         }
     }
+     
+     /** BFS
+     * T: log(n^2), S: log(n)
+     */
+    public int findCircleNum(int[][] M) {
+        int[] visited = new int[M.length];
+        int count = 0;
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 0; i < M.length; i++) {
+            if (visited[i] == 0) {
+                dq.addFirst(i);
+                while (!dq.isEmpty()) {
+                    int n = dq.removeFirst();
+                    visited[n] = 1;
+                    for (int j = 0; j < M.length; j++) {
+                        if (M[n][j] == 1 && visited[j] == 0)
+                            dq.addFirst(j);
+                    }
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+}
+     
    
 }
