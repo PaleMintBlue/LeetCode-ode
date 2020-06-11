f2e71ad193febc09ac3642cbd564992a39d246f6
SwagMint <23306536+PaleMintBlue@users.noreply.github.com>
2020-06-10 21:56:52 -0400

    Update 547. Friend Circles.java

--- Graph/547. Friend Circles.java
+++ Graph/547. Friend Circles.java
@@ -46,7 +46,64 @@ class Solution {
         }
         return count;
     }
-}
+     
+     
+    /** Union-Find
+    * ref: https://blog.csdn.net/guoziqing506/article/details/78752557
+    * T: O(log(n^3)), S: O(1)
+    */
+    class UF {
+        private int[] parent, size;
+        private int count;
+        
+        public UF(int n) {
+            parent = new int[n];
+            size = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+
+        }
+        
+        public int find(int p) {
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+        
+        public void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            if (rootP == rootQ) return;
+            
+            // compress path
+            if (size[rootP] > size[rootQ]) {
+                parent[rootQ] = rootP;
+                size[rootP] += size[rootQ];
+            } else {
+                parent[rootP] = rootQ;
+                size[rootQ] += size[rootP];
+            }
+            count--;
+        }
+        
+        public int count() { return count; }
+    }
+    
+    
+    public int findCircleNum(int[][] M) {
+        int n = M.length;
+        UF uf = new UF(n);
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if (M[i][j] == 1)
+                    uf.union(i, j);
+        return uf.count();
+    }
      
    
 }
