f534a92d8c87db0a1f0e86b7c8e64f770518383f
SwagMint <23306536+PaleMintBlue@users.noreply.github.com>
2020-06-11 23:45:01 -0400

    Update 994. Rotting Oranges.java

--- Graph/994. Rotting Oranges.java
+++ Graph/994. Rotting Oranges.java
@@ -29,5 +29,47 @@ class Solution {
             rotten(grid, i, j + 1, minutes + 1);
         }
     }
+    
+    
+    //BFS
+    public int orangesRotting(int[][] grid) {
+        if(grid.length==0) return -1;
+        
+        int[][] dirs = { {1,0}, {0, 1}, {-1, 0}, {0, -1}};
+        Deque<int[]> dq = new ArrayDeque<>();
+        int fresh = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==2) dq.offer(new int[]{i, j});
+                else if(grid[i][j]==1) fresh++;
+            }
+        }
+       
+        if(fresh==0) return 0;
+        int level = -1;
+        
+        while(!dq.isEmpty()){
+            int size = dq.size();
+            for(int i=0; i<size; i++){
+                int[] c = dq.poll();
+                for(int[] d: dirs){
+                    int x = c[0]+d[0];
+                    int y = c[1]+d[1];
+                    if(x>=0 && y>=0 && x<m && y<n){
+                        if(grid[x][y]==1){
+                            grid[x][y] = 2;     
+                            dq.offer(new int[]{x, y}); 
+                            fresh--;
+                        }   
+                    }
+                }
+            }
+            level++;
+        }
+        return (fresh==0) ? level : -1;
+    }
 
 }
