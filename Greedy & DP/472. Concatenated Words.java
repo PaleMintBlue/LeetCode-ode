6e5383f36cb08e27397947929215cbad4f77ef0e
SwagMint <23306536+PaleMintBlue@users.noreply.github.com>
2020-06-24 03:34:01 -0400

    Create 472. Concatenated Words.java

---
+++ Greedy & DP/472. Concatenated Words.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        List<String> res = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+        for (String word : words) set.add(word);
+        for (String word : words) if (isConcatenated(set, word)) res.add(word);
+        return res;
+    }
+    
+    private boolean isConcatenated(Set<String> set, String s) {
+        for (int i = 1; i < s.length(); i++) {
+            if (set.contains(s.substring(0, i))) {
+                String left = s.substring(i);
+                if (set.contains(left) || isConcatenated(set, left))
+                    return true;
+            }
+        }
+        return false;
+    }
+}
