d7822dbe762ab1a276d929d62c3602a59e94b67f
SwagMint <23306536+PaleMintBlue@users.noreply.github.com>
2020-06-25 02:36:38 -0400

    Create 583. Delete Operation for Two Strings.java

---
+++ Greedy & DP/583. Delete Operation for Two Strings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        return (word1.length() + word2.length() - 2 * common(word1, word2));
+    }
+    
+    public int common(String text1, String text2) {
+        if (text1.length() == 0 || text2.length() == 0) return 0;
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        for (int i = 1; i <= text1.length(); i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+        return dp[text1.length()][text2.length()];
+    }
+}
+
+
