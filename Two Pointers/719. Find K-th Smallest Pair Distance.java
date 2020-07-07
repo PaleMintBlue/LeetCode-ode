b38e93db8bc624890b3353abb1bbf974ff50f48d
SwagMint <23306536+PaleMintBlue@users.noreply.github.com>
2020-07-07 02:16:28 -0400

    Create 719. Find K-th Smallest Pair Distance.java

---
+++ Two Pointers/719. Find K-th Smallest Pair Distance.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int lo = 0;
+        int hi = nums[nums.length - 1] - nums[0];
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            int count = 0, left = 0;
+            for (int right = 0; right < nums.length; ++right) {
+                while (nums[right] - nums[left] > mid) left++;
+                count += right - left;
+            }
+            if (count >= k) hi = mid;
+            else lo = mid + 1;
+        }
+        return lo;
+    }
+}
