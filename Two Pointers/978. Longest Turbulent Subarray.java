76b8d7ff1a41f924faca81c62d45282f6b1ffb2d
SwagMint <23306536+PaleMintBlue@users.noreply.github.com>
2020-07-07 23:23:02 -0400

    Update 978. Longest Turbulent Subarray.java

--- Two Pointers/978. Longest Turbulent Subarray.java
+++ Two Pointers/978. Longest Turbulent Subarray.java
@@ -1,18 +1,41 @@
 class Solution {
     public int maxTurbulenceSize(int[] A) {
+        int up = 1;
+        int maxCount = 0;
+        int curr = 0;
         int N = A.length;
-        int res = 1;
+        if(N < 2) return N;
+        for(int i=0; i < N-1; i++){
+            int k = Integer.compare(A[i], A[i+1]) * up;
+            if (k == 0) curr = 0;
+            else {
+                if(k == 1) {
+                    curr++; 
+                    up *= -1;
+                } else curr = 1;
+            }
+            maxCount = Math.max(curr+1, maxCount);
+        }
+        return maxCount;
+    }
+}
+
+class Solution {
+    public int maxTurbulenceSize(int[] A) {
+        int N = A.length;
+        int ans = 1;
         int anchor = 0;
-        
-        for(int i=1;i<N;i++) {
-            int c = Integer.compare(A[i-1],A[i]); // x<y:-1, x=y:0, x>y:1
-            if(c == 0) anchor = i;
-            else if(i == N-1 || c * Integer.compare(A[i],A[i+1]) != -1) {
-                res = Math.max(res,i-anchor+1);
+
+        for (int i = 1; i < N; ++i) {
+            int c = Integer.compare(A[i-1], A[i]);
+            if (c == 0) {
+                anchor = i;
+            } else if (i == N-1 || c * Integer.compare(A[i], A[i+1]) != -1) {
+                ans = Math.max(ans, i - anchor + 1);
                 anchor = i;
             }
         }
-        
-        return res;
+
+        return ans;
     }
 }
