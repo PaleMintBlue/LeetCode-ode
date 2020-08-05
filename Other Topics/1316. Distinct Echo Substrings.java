008cd97e47ce7c776dc951e304064ba0b6a0fb02
SwagMint <23306536+PaleMintBlue@users.noreply.github.com>
2020-08-05 15:50:28 -0400

    Update 1316. Distinct Echo Substrings.java

--- Other Topics/1316. Distinct Echo Substrings.java
+++ Other Topics/1316. Distinct Echo Substrings.java
@@ -29,3 +29,48 @@ class Solution {
         return (hash[r] - hash[l] * pow[r - l] % MOD + MOD) % MOD;
     }
 }
+
+
+/** beats 96.8% 
+* 103 ms
+*/
+
+class Solution {
+    
+    public int distinctEchoSubstrings(String text) {
+        int n = text.length();
+        long[] hash = new long[n + 1];
+        long[] pow = new long[n + 1];
+        process(text, hash, pow);
+        
+        Set<Integer> res = new HashSet<>();
+        for (int winSize = 1; winSize <= text.length() / 2; winSize++) {
+            int cnt = 0;
+            for (int l = 0, r = winSize; r < text.length(); l++, r++) {
+                if (text.charAt(l) == text.charAt(r)) cnt++;
+                else cnt = 0;
+                if (cnt == winSize) {
+                    res.add(getHash(hash, pow, l + 1, r + 1));
+                    cnt--;
+                }
+            }
+        }
+        return res.size();
+    }
+    
+    private void process(String text, long[] hash, long[] pow) {
+        int mod = 1_000_000_007;
+        pow[0] = 1;
+        for (int i = 0; i < text.length(); i++) {
+            // 0 is not used to represent any character
+            // a -> 1, ... z -> 26
+            hash[i + 1] = (hash[i] * 27 + (text.charAt(i) - 'a' + 1)) % mod;
+            pow[i + 1] = (pow[i] * 27) % mod;
+        }
+    }
+    
+    private int getHash(long[] hash, long[] pow, int l, int r) {
+        int mod = 1_000_000_007;
+        return (int)((hash[r] - hash[l] * pow[r - l] % mod + mod) % mod);
+    }
+}
